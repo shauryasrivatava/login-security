f82b96bf2437c4ee6d37739686d136af7d6caa38
Shaurya Srivastava <shauryasris21hcs@gmail.com>
2022-04-08 02:30:35 +0530

    google authentication updated

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose= require("mongoose");
 const session=require('express-session');
 const passport= require('passport');
 const passportLocalMongoose= require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate=require('mongoose-findorcreate')
 
 const app=express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,17 +28,37 @@ mongoose.connect('mongodb://localhost:27017/userDB');
 
 const userSchema = new mongoose.Schema({ 
     email: String, 
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/login",function(req,res){
     res.render('login');
@@ -49,6 +71,17 @@ app.get("/register",function(req,res){
 app.get("/",function(req,res){
     res.render('home');
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+});
 
 app.get('/secrets',function(req,res){
     if(req.isAuthenticated()){
